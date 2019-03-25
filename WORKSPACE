960de455a7a58c31b6d5f58bdc470928e0b25fe2
Eugene Kliuchnikov <eustas@google.com>
2019-03-25 15:07:53 +0100

    Add simple tests. (#1)

--- WORKSPACE
+++ WORKSPACE
@@ -7,3 +7,28 @@ git_repository(
     remote = "https://github.com/google/brotli.git",
     tag = "v1.0.7",
 )
+
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+http_archive(
+    name = "gtest",
+    url = "https://github.com/google/googletest/archive/release-1.8.1.zip",
+    sha256 = "927827c183d01734cc5cfef85e0ff3f5a92ffe6188e0d18e909c5efebf28a0c7",
+    build_file_content = """
+cc_library(
+    name = "main",
+    srcs = glob(
+        ["src/*.cc"],
+        exclude = ["src/gtest-all.cc"]
+    ),
+    hdrs = glob([
+        "include/**/*.h",
+        "src/*.h"
+    ]),
+    copts = ["-Iexternal/gtest/include"],
+    linkopts = ["-pthread"],
+    visibility = ["//visibility:public"],
+)
+    """,
+    strip_prefix = "googletest-release-1.8.1/googletest",
+)
---
+++ c/tests/bit_reader_test.cc
@@ -0,0 +1,31 @@
+// Copyright (c) Google LLC 2019
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+#include "../dec/bit_reader.h"
+
+#include "gtest/gtest.h"
+#include "../common/types.h"
+
+namespace brunsli {
+
+TEST(BitReader, ReadOneByte) {
+  const uint8_t data[2] = {1, 2};
+  BrunsliBitReader br;
+  // Fill with garbage.
+  for (size_t i = 0; i < sizeof(br.tail_); ++i) {
+    br.tail_[i] = 42;
+  }
+  BrunsliBitReaderInit(&br, data, 1);
+
+  uint32_t firstByte = BrunsliBitReaderReadBits(&br, 8);
+  ASSERT_EQ(1u, firstByte);
+
+  // It is illegal to read past input, but bit reader gives us some guarantees.
+  uint32_t secondByte = BrunsliBitReaderReadBits(&br, 8);
+  ASSERT_EQ(0u, secondByte);
+}
+
+}  // namespace brunsli
