7648fbee979dd53e169180149a6d326cba73f9e9
Christian Schneider <schnaader@gmx.de>
2019-05-21 09:03:11 +0200

    Changes to enable compiling with MSVC
    - Don't stop if not clang, but recommend it with a warning
    - Use clang specific settings only when clang is used

--- brunsli.cmake
+++ brunsli.cmake
@@ -58,101 +58,104 @@ target_link_libraries(brunslienc-static PRIVATE
 )
 
 foreach(lib brunslicommon-static brunslidec-static brunslienc-static)
-  target_compile_options(${lib} PUBLIC
-    # Debug flags
-    -dwarf-column-info
-    -debug-info-kind=line-tables-only
-    -dwarf-version=4
-    -debugger-tuning=gdb
+  if("${CMAKE_C_COMPILER_ID}" MATCHES "Clang" OR
+     "${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
+    target_compile_options(${lib} PUBLIC
+      # Debug flags
+      -dwarf-column-info
+      -debug-info-kind=line-tables-only
+      -dwarf-version=4
+      -debugger-tuning=gdb
 
-    # F_FLAGS
-    -fmerge-all-constants
-    -fno-builtin-fwrite
-    -fno-builtin-fread
-    -fno-signed-char
-    -fsized-deallocation
-    -fnew-alignment=8
-    -fno-cxx-exceptions
-    -fno-exceptions
-    -fno-slp-vectorize
-    -fno-vectorize
+      # F_FLAGS
+      -fmerge-all-constants
+      -fno-builtin-fwrite
+      -fno-builtin-fread
+      -fno-signed-char
+      -fsized-deallocation
+      -fnew-alignment=8
+      -fno-cxx-exceptions
+      -fno-exceptions
+      -fno-slp-vectorize
+      -fno-vectorize
 
-    # WARN_FLAGS
-    -Wformat-security
-    -Wno-char-subscripts
-    -Wno-error=deprecated-declarations
-    -Wno-sign-compare
-    -Wno-strict-overflow
-    -Wno-unused-function
-    -Wthread-safety-analysis
-    -Wno-unknown-warning-option
-    -Wno-unused-command-line-argument
-    -Wno-ignored-optimization-argument
-    -Wno-ambiguous-member-template
-    -Wno-pointer-sign
-    -Wno-address-of-packed-member
-    -Wno-enum-compare-switch
-    -Wno-expansion-to-defined
-    -Wno-extern-c-compat
-    -Wno-gnu-alignof-expression
-    -Wno-gnu-designator
-    -Wno-gnu-variable-sized-type-not-at-end
-    -Wno-ignored-attributes
-    -Wno-ignored-qualifiers
-    -Wno-inconsistent-missing-override
-    -Wno-invalid-source-encoding
-    -Wno-mismatched-tags
-    -Wno-potentially-evaluated-expression
-    -Wno-return-std-move
-    -Wno-self-assign-overloaded
-    -Wno-tautological-constant-compare
-    -Wno-tautological-constant-in-range-compare
-    -Wno-tautological-type-limit-compare
-    -Wno-tautological-undefined-compare
-    -Wno-tautological-unsigned-zero-compare
-    -Wno-tautological-unsigned-enum-zero-compare
-    -Wno-undefined-func-template
-    -Wno-unknown-pragmas
-    -Wno-unused-const-variable
-    -Wno-unused-lambda-capture
-    -Wno-unused-local-typedef
-    -Wno-unused-private-field
-    -Wno-private-header
-    -Wfloat-overflow-conversion
-    -Wfloat-zero-conversion
-    -Wfor-loop-analysis
-    -Wgnu-redeclared-enum
-    -Winfinite-recursion
-    -Wliteral-conversion
-    -Wself-assign
-    -Wstring-conversion
-    -Wtautological-overlap-compare
-    -Wunused-comparison
-    -Wvla
-    -Wno-reserved-user-defined-literal
-    -Wno-return-type-c-linkage
-    -Wno-deprecated
-    -Wno-invalid-offsetof
-    -Wno-literal-suffix
-    -Woverloaded-virtual
-    -Wnon-virtual-dtor
-    -Wdeprecated-increment-bool
-    -Wc++11-compat
-    -Wno-c++11-compat-binary-literal
-    -Wc++2a-extensions
-    -Wno-register
-    -Wno-dynamic-exception-spec
-    -Wprivate-header
-    -Wno-builtin-macro-redefined
+      # WARN_FLAGS
+      -Wformat-security
+      -Wno-char-subscripts
+      -Wno-error=deprecated-declarations
+      -Wno-sign-compare
+      -Wno-strict-overflow
+      -Wno-unused-function
+      -Wthread-safety-analysis
+      -Wno-unknown-warning-option
+      -Wno-unused-command-line-argument
+      -Wno-ignored-optimization-argument
+      -Wno-ambiguous-member-template
+      -Wno-pointer-sign
+      -Wno-address-of-packed-member
+      -Wno-enum-compare-switch
+      -Wno-expansion-to-defined
+      -Wno-extern-c-compat
+      -Wno-gnu-alignof-expression
+      -Wno-gnu-designator
+      -Wno-gnu-variable-sized-type-not-at-end
+      -Wno-ignored-attributes
+      -Wno-ignored-qualifiers
+      -Wno-inconsistent-missing-override
+      -Wno-invalid-source-encoding
+      -Wno-mismatched-tags
+      -Wno-potentially-evaluated-expression
+      -Wno-return-std-move
+      -Wno-self-assign-overloaded
+      -Wno-tautological-constant-compare
+      -Wno-tautological-constant-in-range-compare
+      -Wno-tautological-type-limit-compare
+      -Wno-tautological-undefined-compare
+      -Wno-tautological-unsigned-zero-compare
+      -Wno-tautological-unsigned-enum-zero-compare
+      -Wno-undefined-func-template
+      -Wno-unknown-pragmas
+      -Wno-unused-const-variable
+      -Wno-unused-lambda-capture
+      -Wno-unused-local-typedef
+      -Wno-unused-private-field
+      -Wno-private-header
+      -Wfloat-overflow-conversion
+      -Wfloat-zero-conversion
+      -Wfor-loop-analysis
+      -Wgnu-redeclared-enum
+      -Winfinite-recursion
+      -Wliteral-conversion
+      -Wself-assign
+      -Wstring-conversion
+      -Wtautological-overlap-compare
+      -Wunused-comparison
+      -Wvla
+      -Wno-reserved-user-defined-literal
+      -Wno-return-type-c-linkage
+      -Wno-deprecated
+      -Wno-invalid-offsetof
+      -Wno-literal-suffix
+      -Woverloaded-virtual
+      -Wnon-virtual-dtor
+      -Wdeprecated-increment-bool
+      -Wc++11-compat
+      -Wno-c++11-compat-binary-literal
+      -Wc++2a-extensions
+      -Wno-register
+      -Wno-dynamic-exception-spec
+      -Wprivate-header
+      -Wno-builtin-macro-redefined
 
-    # Language flags
-    -disable-free
-    -disable-llvm-verifier
-    -discard-value-names
-    # Note: this works only because this is the only -Xclang passed.
-    -Xclang -relaxed-aliasing
-    -fmath-errno
-  )
+      # Language flags
+      -disable-free
+      -disable-llvm-verifier
+      -discard-value-names
+      # Note: this works only because this is the only -Xclang passed.
+      -Xclang -relaxed-aliasing
+      -fmath-errno
+    )
+  endif()
 
   target_include_directories(${lib}
     PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}")
