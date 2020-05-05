f8785f8b7e6c5154d2926ecd73cfe59b820c2bc0
Eugene Kliuchnikov <eustas.ru@gmail.com>
2020-05-05 14:08:33 +0200

    Improve first-load SW experience. (#77)

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -1,6 +1,38 @@
 #!/bin/sh
 
-function compressOne() {
+function updateBr() {
+  local ORIG="$1"
+  local BR="${ORIG}.br"
+  local TMP="$(mktemp)"
+  local FAIL=1
+  brotli -dfk "${BR}" -o "${TMP}" >/dev/null 2>/dev/null
+  cmp -s "${ORIG}" "${TMP}" && FAIL=0
+  rm -f "${TMP}" >/dev/null 2>/dev/null
+  [[ ${FAIL} = 0 ]] && return
+  ORIG_SIZE=`stat -f %z ${ORIG}`
+  echo "Creating ${BR}"
+  brotli -Zfk "${ORIG}"
+  BR_SIZE=`stat -f %z "${BR}"`
+  [[ $BR_SIZE -ge $ORIG_SIZE ]] && rm "${BR}"
+}
+
+function updateGz() {
+  local ORIG="$1"
+  local GZ="${ORIG}.gz"
+  local TMP="$(mktemp)"
+  local FAIL=1
+  gzip -cdf "${GZ}" > "${TMP}" 2>/dev/null
+  cmp -s "${ORIG}" "${TMP}" && FAIL=0
+  rm -f "${TMP}" >/dev/null 2>/dev/null
+  [[ ${FAIL} = 0 ]] && return
+  ORIG_SIZE=`stat -f %z ${ORIG}`
+  echo "Creating ${GZ}"
+  zopfli --i500 "${ORIG}"
+  GZ_SIZE=`stat -f %z "${GZ}"`
+  [[ $GZ_SIZE -ge $ORIG_SIZE ]] && rm "${GZ}"
+}
+
+function updateFile() {
   FILE=$1
   case "${FILE##*.}" in
     "br") exit 0;;
@@ -9,16 +41,12 @@ function compressOne() {
     "jpg") exit 0;;
     "j") exit 0;;
   esac
-  echo "Compressing $FILE"
-  ORIG_SIZE=`stat -f %z $FILE`
-  brotli -Zfk $FILE
-  BR_SIZE=`stat -f %z $FILE.br`
-  zopfli --i500 $FILE
-  GZ_SIZE=`stat -f %z $FILE.gz`
-  if [ $BR_SIZE -ge $ORIG_SIZE ]; then rm $FILE.br; fi
-  if [ $GZ_SIZE -ge $ORIG_SIZE ]; then rm $FILE.gz; fi
+  updateBr "${FILE}"
+  updateGz "${FILE}"
 }
 
-export -f compressOne
+export -f updateBr
+export -f updateGz
+export -f updateFile
 all_files=`ls docs/*`
-echo $all_files | xargs -n 1 -P `nproc` bash -c 'compressOne "$@"' _
+echo $all_files | xargs -n 1 -P `nproc` bash -c 'updateFile "$@"' _
